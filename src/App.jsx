50616534b587fd6b2d5a6047759b4c6e626cdf3e
Serb <SerbCSharp@mail.ru>
2025-07-30 21:28:44 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ const medium = import('@pmndrs/assets/fonts/inter_medium.woff')
 export const App = () => (
   <Canvas flat camera={{ fov: 75, position: [0, 0, 20] }} eventSource={document.getElementById('root')} eventPrefix="client">
     <color attach="background" args={['#f0f0f0']} />
-    <Frame id="01" name={`pick\nles`} author="Omar Faruq Tawsif" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
-      <Gltf src="fiesta_tea-transformed.glb" scale={8} position={[0, -0.7, -2]} />
+    <Frame id="01" name="ThreeJs" author="Sergey Belyakov" bg="#e4cdac" position={[-1.15, 0, 0]} rotation={[0, 0.5, 0]}>
+      {/* <Gltf src="fiesta_tea-transformed.glb" scale={8} position={[0, -0.7, -2]} /> */}
     </Frame>
-    <Frame id="02" name="tea" author="Omar Faruq Tawsif">
-      <Gltf src="fiesta_tea-transformed.glb" position={[0, -2, -3]} />
+    <Frame id="02" name="Portfolio" author="Sergey Belyakov" bg="#d1d1ca">
+      {/* <Gltf src="fiesta_tea-transformed.glb" position={[0, -2, -3]} /> */}
     </Frame>
-    <Frame id="03" name="still" author="Omar Faruq Tawsif" bg="#d1d1ca" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]}>
-      <Gltf src="fiesta_tea-transformed.glb" scale={2} position={[0, -0.8, -4]} />
+    <Frame id="03" name="R3F" author="Sergey Belyakov" bg="#e4cdac" position={[1.15, 0, 0]} rotation={[0, -0.5, 0]}>
+      {/* <Gltf src="fiesta_tea-transformed.glb" scale={2} position={[0, -0.8, -4]} /> */}
     </Frame>
     <Rig />
     <Preload all />
@@ -36,7 +36,7 @@ function Frame({ id, name, author, bg, width = 1, height = 1.61803398875, childr
   useFrame((state, dt) => easing.damp(portal.current, 'blend', params?.id === id ? 1 : 0, 0.2, dt))
   return (
     <group {...props}>
-      <Text font={suspend(medium).default} fontSize={0.3} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
+      <Text font={suspend(medium).default} fontSize={0.1} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
         {name}
       </Text>
       <Text font={suspend(regular).default} fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>
