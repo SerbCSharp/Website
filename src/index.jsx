63a50976499c54e3d17fd3e831ab2756ea6c2b52
Serb <SerbCSharp@mail.ru>
2025-07-30 20:08:46 +0300

    Nodebox

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,28 @@
-import './style.css'
 import { createRoot } from 'react-dom/client'
+import { useRoute, useLocation } from 'wouter'
+import { Logo } from '@pmndrs/branding'
+import { App } from './App'
 
-const root = createRoot(document.querySelector('#root'))
+function Root() {
+  const [, params] = useRoute('/item/:id')
+  const [, setLocation] = useLocation()
+  return (
+    <>
+      <App />
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+        <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+          pmnd.rs
+          <br />
+          dev collective
+        </a>
+        <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>15/06/2023</div>
+        <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
+        <a style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }} href="#" onClick={() => setLocation('/')}>
+          {params ? '< back' : 'double click to enter portal'}
+        </a>
+      </div>{' '}
+    </>
+  )
+}
 
-root.render(
-    <h1>Hello React</h1>
-)
+createRoot(document.getElementById('root')).render(<Root />)
