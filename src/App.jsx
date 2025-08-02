768ee0c6be7249ffa31873b4ad622f3ece3dfc3c
Serb <SerbCSharp@mail.ru>
2025-08-02 11:55:36 +0300

    Add man

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import * as THREE from 'three'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
-import { useCursor, MeshPortalMaterial, CameraControls, Gltf, Text, Preload } from '@react-three/drei'
+import { useCursor, useTexture, CameraControls, Gltf, Text } from '@react-three/drei'
 
 export default function App()
 {
   return (
   <Canvas flat camera={{ fov: 75, position: [0, 0, 20] }} eventSource={document.getElementById('root')} eventPrefix="client">
-    <color attach="background" args={['#f0f0f0']} />
-    <Frame id="JS" name="ThreeJs" bg="#d1d1ca" url="https://three-js-eight-henna.vercel.app/" position={[-2.3, 0, 0]} />
-    <Frame id="C#" name="Microservice" bg="#e4cdac" url="https://github.com/SerbCSharp/Catalog" position={[-1.15, 0, 0]} />
-    <Frame id="JS" name="Portfolio" bg="#d1d1ca" url="https://portfolio-eta-five-24.vercel.app/" />
-    <Frame id="C#" name="TelegramBot" bg="#e4cdac" url="https://github.com/SerbCSharp/TelegramChatBot" position={[1.15, 0, 0]} />
-    <Frame id="JS" name="R3F" bg="#d1d1ca" url="https://r3-f-three.vercel.app/" position={[2.3, 0, 0]} />
+    <color attach="background" args={['slategrey']} />
+    <Frame id="JS" name="ThreeJs" bg="./textures/Portal.JPG" url="https://three-js-eight-henna.vercel.app/" position={[-2.3, 0, 0]} rotation={[0, 0, 0]} />
+    <Frame id="C#" name="Microservice" bg="./textures/Microservice.JPG" url="https://github.com/SerbCSharp/Catalog" position={[-1.15, 0, 0]} />
+    <Frame id="JS" name="Portfolio" bg="./textures/Portfolio.JPG" url="https://portfolio-eta-five-24.vercel.app/" position={[0, 0, 0]} />
+    <Frame id="C#" name="TelegramBot" bg="./textures/TelegramBot.JPG" url="https://github.com/SerbCSharp/TelegramChatBot" position={[1.15, 0, 0]} />
+    <Frame id="JS" name="R3F" bg="./textures/R3F.JPG" url="https://r3-f-three.vercel.app/" position={[2.3, 0, 0]} rotation={[0, 0, 0]} />
     <Rig />
+          <Gltf src="rp_dennis_posed_004_30k.glb" scale={0.5} position={[0.6, -0.7, 0.5]} />
   </Canvas>
   )
 }
 
-function Frame({ id, name, bg, url, width = 1, height = 1.61803398875, ...props }) {
+function Frame({ id, name, bg, url, width = 1.075, height = 1.61803398875, ...props }) {
   const [hovered, hover] = useState(false)
   useCursor(hovered)
   return (
     <group {...props}>
-      <Text fontSize={0.1} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>{name}</Text>
-      <Text fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>{id}</Text>
-      <Text fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>"Sergey Belyakov"</Text>
+
+              <directionalLight position={ [ 0, 0, 2 ] } intensity={ 0.8 } />
+
+      <Text color='slategrey' fontSize={0.1} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>{name}</Text>
+      <Text color='slategrey' fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>{id}</Text>
+      <Text color='slategrey' fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>Sergey Belyakov</Text>
         <mesh onClick={ x => window.location.href = url } onPointerOver={(e) => hover(true)} onPointerOut={() => hover(false)}>
-            <planeGeometry args={[width, height]} />
-        <MeshPortalMaterial>
-          <color attach="background" args={[bg]} />
-        </MeshPortalMaterial>
+          <planeGeometry args={[width, height]} />
+          <meshStandardMaterial map={ useTexture(bg) } side={THREE.DoubleSide}>
+            {/* <color attach="background" args={[bg]} /> */}
+          </meshStandardMaterial>
         </mesh>
     </group>
   )
