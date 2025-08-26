2806df971c77297add2e9a88794e3c3a98bac673
Serb <SerbCSharp@mail.ru>
2025-08-26 09:16:14 +0300

    Вернул await

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function Frame({ id, name, bg, url, width = 1.075, height = 1.61803398875, ...pr
       url: url
     }
 
-    const response = fetch('https://repository.somee.com/SiteVisits/Create',
+    await fetch('https://repository.somee.com/SiteVisits/Create',
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json;charset=utf-8' },
